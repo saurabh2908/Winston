553282698df7df54d23aeb889676169f3bda3b18
Saurabh singh <2908aman@gmail.com>
2019-08-02 00:04:39 +0530

    Use of Winston

--- Loggers/db/models/user.js
+++ Loggers/db/models/user.js
@@ -1,7 +1,7 @@
 const connection = require('../connection');
 const Schema = connection.Schema;
 const userSchema = new Schema({
-    'userid':{type:String, required:true, unique:true},
+    'userid':{type:String, required:true},
     'password':{type:Number, required:true}
  
 });
